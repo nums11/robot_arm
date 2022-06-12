4901a014d09ffd312b63ca97658fbf81fe53d7cc
Numfor Tiapo <numsmt2@gmail.com>
2022-06-12 15:58:43 -0700

    Stuff

---
+++ tf_pose_classifier.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import pathlib
+
+data_dir = os.path.join(os.getcwd(), 'data')
+
+# data_dir = '/home/nums/robot_arm/data/pos'
+# # image_count = len(list(data_dir.glob('*/*.jpg')))
+
+# # pos = list(data_dir.glob(r'pos/*'))
+# # PIL.Image.open('./data/pos/pos.jpg')
+# # print("done")
+
+batch_size = 1
+img_height = 256
+img_width = 256
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir, labels='inferred')
+class_names = train_ds.class_names
+
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_ds.take(1):
+#   for i in range(6):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(images[i].numpy().astype("uint8"))
+#     plt.title(class_names[labels[i]])
+#     plt.axis("off")
+
+# plt.show()
+
+num_classes = len(class_names)
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+model.summary()
+
+epochs=20
+history = model.fit(
+  train_ds,
+  epochs=epochs
+)
+
+model.save('tf_pose_classifier_v1')
+
+acc = history.history['accuracy']
+loss = history.history['loss']
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training Loss')
+plt.show()
+
+
+img_path = os.path.join(os.getcwd(), 'data/pos/pos.jpg')
+img = tf.keras.utils.load_img(
+    img_path, target_size=(img_height, img_width)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
