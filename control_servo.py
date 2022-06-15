4b4416328c54aa11a85f1d622c25f5b3ec4da1c7
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2022-06-14 17:15:47 -0700

    Add code for controlling servo

---
+++ control_servo.py
@@ -0,0 +1,21 @@
+from pyfirmata import Arduino, SERVO, util
+from time import sleep
+
+port = '/dev/ttyACM0'
+pin = 9
+board = Arduino(port)
+
+board.digital[pin].mode = SERVO
+
+def rotateServo(pin, angle):
+	board.digital[pin].write(angle)
+	sleep(0.015)
+
+while True:
+	for i in range(0, 180):
+		rotateServo(pin, i)
+		print(i)
+
+	for i in reversed(range(0, 180)):
+		rotateServo(pin, i)
+		print(i)
