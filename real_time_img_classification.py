ba5bf28ee66fb14a41d9ea8d36a173fb1cb80b64
Numfor Tiapo <numsmt2@gmail.com>
2022-07-19 09:39:10 -0700

    Train new image classification model

--- real_time_img_classification.py
+++ real_time_img_classification.py
@@ -3,34 +3,6 @@ import os
 import numpy as np
 import cv2
 
-# model = tf.keras.models.load_model('tf_pose_classifier_v3.h5')
-
-# img_path = '/home/nums/Pictures/Webcam/neg2.jpg'
-# print(img_path)
-# print('\n')
-# img_height = 256
-# img_width = 256
-# img = tf.keras.utils.load_img(
-#     img_path, target_size=(img_height, img_width)
-# )
-# img_array = tf.keras.utils.img_to_array(img)
-# img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-# predictions = model.predict(img_array)
-# print("predictions")
-# print(predictions)
-# score = tf.nn.softmax(predictions[0])
-# print("score", score)
-
-# class_names = ['neg','pos']
-# print(
-#     "This image most likely belongs to {} with a {:.2f} percent confidence."
-#     .format(class_names[np.argmax(score)], 100 * np.max(score))
-# )
-
-# I think the model just learned to always predict negative. Need to trainf for higher accuracy. (83% is 5/6)
-# Fixed by just training longer now add real-time component
-
 def predictImageClass(img):
     model = tf.keras.models.load_model('tf_pose_classifier_v4.h5')
 
